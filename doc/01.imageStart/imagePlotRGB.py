14421de5edbc69798b04e6292c52a08e54fca176
2yongbum <32673738+2yongbum@users.noreply.github.com>
2023-01-08 20:57:12 +0900

    Update imagePlotRGB.py
    COLOR_BGR2RGB for matplotlib

--- doc/01.imageStart/imagePlotRGB.py
+++ doc/01.imageStart/imagePlotRGB.py
@@ -3,11 +3,8 @@ import cv2
 from matplotlib import pyplot as plt # as는 alias 적용시 사용
 
 img = cv2.imread('lena.jpg', cv2.IMREAD_COLOR)
+img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-b, g, r = cv2.split(img)   # img파일을 b,g,r로 분리
-img2 = cv2.merge([r,g,b]) # b, r을 바꿔서 Merge
-
+plt.axis('off') # 창의 x축 y축 제거
 plt.imshow(img2)
-plt.xticks([]) # x축 눈금
-plt.yticks([]) # y축 눈금
 plt.show()
